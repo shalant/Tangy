b16d7dec382f5c1406436cd5d53df080abcbf2be
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-12-07 15:55:12 -0600

    building up product playground

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -12,5 +12,7 @@ namespace Tangy_Models.LearnBlazorModels
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
+
+        public List<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
