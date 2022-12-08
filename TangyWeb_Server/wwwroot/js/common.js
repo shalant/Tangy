a351a3e5b8a5dcac02e6198d3eeb44fe4786eac6
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-12-08 16:59:35 -0600

    learning about lifecycle

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -1,7 +1,24 @@
 ﻿window.ShowToastr = (type, message) => {
     if (type === "success") {
         toastr.success(message, "Operation Successful", { timeOut: 5000 });
-    }if (type === "error") {
+    } if (type === "error") {
         toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
     }
 }
