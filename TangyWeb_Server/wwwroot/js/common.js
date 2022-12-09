c71f3dc888fe8133e6db80daea57bb0510dc5119
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-12-09 17:01:26 -0600

    got delete functionality, optimizing

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -22,3 +22,11 @@ window.ShowSwal = (type, message) => {
         )
     }
 }
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
