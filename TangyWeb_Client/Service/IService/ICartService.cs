438b5203bdc2be25b7a4bc8687dba2e0bda653d3
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-12-12 15:33:18 -0600

    Got shopping cart to use local storage

---
+++ TangyWeb_Client/Service/IService/ICartService.cs
@@ -0,0 +1,10 @@
+﻿using TangyWeb_Client.ViewModels;
+
+namespace TangyWeb_Client.Service.IService
+{
+    public interface ICartService
+    {
+        Task DecrementCart(ShoppingCart choppingCart);
+        Task IncrementCart(ShoppingCart choppingCart);
+    }
+}
